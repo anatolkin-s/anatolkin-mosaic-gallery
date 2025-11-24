c6761f470085f2ff56b84eae547541a786b694b6
Sergey Fedorov <typo3@anatolkin.com>
2025-11-23 20:37:22 -0500

    Meta: align composer.json and ext_emconf.php for v0.1.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,8 @@
 <?php
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Anatolkin Mosaic Gallery',
-    'description' => 'Masonry-like image gallery for TYPO3 using FAL with optional lightbox.',
+    'description' => 'Masonry-like image gallery for TYPO3 using FAL with optional GLightbox.',
     'category' => 'plugin',
     'author' => 'Sergey Fedorov',
     'author_email' => 'typo3@anatolkin.com',
@@ -10,10 +11,11 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.6',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0-13.99.99',
+            'typo3' => '13.4.0-13.99.99',
             'php'   => '8.1.0-8.3.99',
         ],
         'conflicts' => [],
         'suggests'  => [],
     ],
 ];
+
