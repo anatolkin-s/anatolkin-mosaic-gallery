195d6c73da08f022f4f682320600314175b3e59a
Sergey Fedorov <typo3@anatolkin.com>
2025-11-25 22:52:22 -0500

    Refactor TCA: use EXT:anatolkin_mosaic_gallery for FlexForm path

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,12 +3,11 @@ declare(strict_types=1);
 
 defined('TYPO3') || die();
 
-use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 (static function (): void {
     // Extension key of this plugin
-    $extensionKey = 'mosaic_gallery';
+    $extensionKey = 'anatolkin_mosaic_gallery';
 
     // Plugin name as defined in registerPlugin()
     $pluginName = 'Pi1';
@@ -26,22 +25,19 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
     // Register "Anatolkin Mosaic Gallery" in the list of plugins (CType = list, field list_type)
     $GLOBALS['TCA']['tt_content']['columns']['list_type']['config']['items'][] = [
-        'Anatolkin Mosaic Gallery', // Label shown in the BE content type selector
-        $pluginSignature,           // list_type value, e.g. anatolkinmosaicgallery_pi1
-        'mosaic-gallery-plugin',    // iconIdentifier (defined in ext_localconf.php)
+        'Anatolkin Mosaic Gallery',   // Label shown in the BE content type selector
+        $pluginSignature,             // list_type value, e.g. anatolkinmosaicgallery_pi1
+        'mosaic-gallery-plugin',      // iconIdentifier (registered in ext_localconf.php)
     ];
 
     // Enable FlexForm configuration for this plugin
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature]
         = 'pi_flexform';
 
-    // Absolute path to FlexForm XML from the project root
-    $flexFormPath = Environment::getProjectPath()
-        . '/packages/mosaic_gallery/Configuration/FlexForms/MosaicGallery.xml';
-
+    // FlexForm XML by extension key – работает и в Composer, и в Classic установках
     ExtensionManagementUtility::addPiFlexFormValue(
         $pluginSignature,
-        'FILE:' . $flexFormPath
+        'FILE:EXT:anatolkin_mosaic_gallery/Configuration/FlexForms/MosaicGallery.xml'
     );
 })();
 
