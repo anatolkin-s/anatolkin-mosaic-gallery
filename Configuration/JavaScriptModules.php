3f6a7328aed41c847dd737f19f353d1cc6f3e792
anatolkin-s <typo3@anatolkin.com>
2026-08-14 19:33:52 -0400

    fix: register metadata editor for backend forms

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 return [
     'dependencies' => ['backend'],
+    'tags' => [
+        'backend.form',
+    ],
     'imports' => [
         '@anatolkin/mosaic-gallery/' => 'EXT:anatolkin_mosaic_gallery/Resources/Public/JavaScript/',
     ],
